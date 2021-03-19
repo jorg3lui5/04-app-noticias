078fe4b6681e87458477732259ebed329fc5c08d
jorg3lui5 <jorg3lui5@hotmail.com>
2021-03-18 21:39:05 -0500

    Mostrar encabezados dependiendo de la categoría

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { IonSegment } from '@ionic/angular';
 import { NoticiasService } from '../../services/noticias.service';
+import { Article } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab2',
@@ -8,6 +9,8 @@ import { NoticiasService } from '../../services/noticias.service';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page implements OnInit{
+
+  noticias: Article[]= [];
 
   @ViewChild(IonSegment, { static: true }) segment: IonSegment;
 
@@ -19,9 +22,20 @@ export class Tab2Page implements OnInit{
 
   ngOnInit(){
     this.segment.value=this.categorias[0];
-    this._noticiasService.getTopHeadLinesCategoria(this.categorias[0])
+    this.cargarNoticias(this.categorias[0]);
+  }
+
+  cambioCategoria(event){
+    console.log(event.detail.value)
+    this.noticias=[];
+    this.cargarNoticias(event.detail.value);
+  }
+
+  cargarNoticias(categoria: string){
+    this._noticiasService.getTopHeadLinesCategoria(categoria)
     .subscribe(resp=>{
       console.log('noticias', resp.articles);
+      this.noticias.push(...resp.articles);
     });
   }
 }
