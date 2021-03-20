6fa4dcf246cee448333b6a21ae4ffbf4928f13f1
jorg3lui5 <jorg3lui5@hotmail.com>
2021-03-20 18:08:25 -0500

    Mostrar action sheet en las noticia

--- src/app/componentes/noticia/noticia.component.ts
+++ src/app/componentes/noticia/noticia.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-noticia',
@@ -13,7 +14,8 @@ export class NoticiaComponent implements OnInit {
   @Input() indice: number;
   
   constructor(
-    private iab: InAppBrowser
+    private iab: InAppBrowser,
+    public actionSheetController: ActionSheetController
   ) { }
 
   ngOnInit() {}
@@ -24,4 +26,34 @@ export class NoticiaComponent implements OnInit {
     //abre correctamenrte dentro de la app como debe de ser. Si quiero que aparezca la barra de navegacion, cambiar location:yes
     //this.iab.create(this.noticia.url,'_blank',{location: 'no'});
   }
+  async lanzarMenu(){
+    const actionSheet = await this.actionSheetController.create({
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Compartir',
+        icon: 'share',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Favorito',
+        icon: 'star',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('favorito');
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
 }
